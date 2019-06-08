1a6f898793b8e7ead2344da489edcb2f25841313
GMNSA <kohotokk@mail.ru>
2019-06-08 23:34:58 +0000

    exercise 1.20

---
+++ ex1.20/srcs/main.c
@@ -0,0 +1,47 @@
+/* main.c */
+
+#include "../includes/tools.h"
+
+int     n = 8;
+
+// ----------------------------------------------------------------------
+
+void    detab(void);
+
+// ----------------------------------------------------------------------
+
+int     main(void)
+{
+    detab();
+    return (0);
+}
+
+// ----------------------------------------------------------------------
+
+/**
+ * @brief void  detab(void) - Replace tab on spaces and print them.
+ *
+ */
+void    detab(void)
+{
+    int     c;
+    int     nc = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c != '\t' && c != '\n')
+            nc = (nc + 1) % n;
+
+        if (c == '\n')
+            nc = 0;
+
+        if (c == '\t')
+        {
+            for (int i = 0; i < (n - nc); ++i)
+                putchar('-');
+            nc = 0;
+        }
+        else
+            putchar(c);
+    }
+}
